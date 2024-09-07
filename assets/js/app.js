806a82febd334a78d9397c45125ba1c14de8d521
Kevin Stradtman <stradtkt22@gmail.com>
2024-09-07 14:13:07 -0400

    Added the functionality to populate fields in the html

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,15 +28,30 @@ const addTransactionDOM = (transaction) => {
     `;
     list.appendChild(item);
 }
-//////////////////////////////////////////////////
-// Application Event Listeners
-//////////////////////////////////////////////////
-
+const updateValues = () => {
+    const amounts = transactions
+        .map((transaction) => transaction.amount);
+    const total = amounts
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+    const expense = (amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc += item), 0) * -1)
+        .toFixed(2);
+    balance.innerText = `$${total}`;
+    moneyPlus.innerText = `$${income}`;
+    moneyMinus.innerText = `$${expense}`;
+}
 //////////////////////////////////////////////////
 // Application Initializer
 //////////////////////////////////////////////////
 const init = () => {
     list.innerHTML = '';
     transactions.forEach(addTransactionDOM);
+    updateValues();
 }
 init();
